705889f77e4e29594ac544cff4375ee2c8f342a9
khlil2020 <77234075+khlil2020@users.noreply.github.com>
2021-05-23 14:47:52 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,65 +1,21 @@
-
+import logo from './logo.svg';
 import './App.css';
-import { Button, FormControl,InputGroup} from 'react-bootstrap';
-
-
+import Img from './imageInSrc.jpg'
 function App() {
   return (
     <div className="App">
-      <h1>
-        Form
-      </h1>
- 
-  <InputGroup className="mb-3">
-    <InputGroup.Prepend>
-      <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-    </InputGroup.Prepend>
-    <FormControl
-      placeholder="Username"
-      aria-label="Username"
-      aria-describedby="basic-addon1"
-    />
-  </InputGroup>
+      
+      <>
+  <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+    <h1 className="title red">Khalil Ben Zina</h1>
+    <br />
+    <img src={Img}/>
+    <br />
+    <img src="/imageInPublic.jpg" />
+  </div>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/7fPXI_MnBOY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</>
 
-  <InputGroup className="mb-3">
-    <FormControl
-      placeholder="Recipient's username"
-      aria-label="Recipient's username"
-      aria-describedby="basic-addon2"
-    />
-    <InputGroup.Append>
-      <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
-    </InputGroup.Append>
-  </InputGroup>
-
-  <label htmlFor="basic-url">Your vanity URL</label>
-  <InputGroup className="mb-3">
-    <InputGroup.Prepend>
-      <InputGroup.Text id="basic-addon3">
-        https://example.com/users/
-      </InputGroup.Text>
-    </InputGroup.Prepend>
-    <FormControl id="basic-url" aria-describedby="basic-addon3" />
-  </InputGroup>
-
-  <InputGroup className="mb-3">
-    <InputGroup.Prepend>
-      <InputGroup.Text>+216</InputGroup.Text>
-    </InputGroup.Prepend>
-    <FormControl aria-label="Amount (to the nearest dollar)" />
-    <InputGroup.Append>
-     
-    </InputGroup.Append>
-  </InputGroup>
-
-  <InputGroup>
-    <InputGroup.Prepend>
-      <InputGroup.Text>Describe yourself</InputGroup.Text>
-    </InputGroup.Prepend>
-    <FormControl as="textarea" aria-label="With textarea" />
-  </InputGroup>
-
-    
     </div>
   );
 }
